369eb986c3e05d69e148c70be4dc884cd9cd7567
RahulRMandre <rahulmandre74@gmail.com>
2021-06-01 23:58:28 +0530

    Log

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.androidfundamentals
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG,"HelloWorld")
+
     }
 }
