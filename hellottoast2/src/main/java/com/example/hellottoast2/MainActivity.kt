51405605b648d0d9d5685d9cd1148d2e418c2c3c
RahulRMandre <rahulmandre74@gmail.com>
2021-06-03 00:16:34 +0530

    toast app part2

---
+++ hellottoast2/src/main/java/com/example/hellottoast2/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.hellottoast2
+
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
+    private var count=0
+    private lateinit var mShowCount:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG,"HelloWorld")
+
+        mShowCount =  findViewById(R.id.show_count)
+
+    }
+
+    fun showToast(view: View) {
+        val toast: Toast = Toast.makeText(
+            this, R.string.toast_message,
+            Toast.LENGTH_SHORT
+        )
+        toast.show()
+
+    }
+
+    fun countUp(view: View) {
+        count++
+        mShowCount.text=count.toString()
+    }
+}
